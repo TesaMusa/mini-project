ddb9f6e7221c22c29f15b4c6913d5f3bd7b0325a
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-06 00:01:41 +0100

    Add navbar,sidebar,footer, day1 iterations done

---
+++ e-commerce-spa/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavBar(){
+   return (
+    <>
+ <nav className="navbar">
+  <div className="logo">
+    <img src="./src/assets/elogo-bg.png" alt="logo"/>
+    <div className="text">    Angry Cats Emporium
+    </div>
+  </div>
+</nav>
+     
+   </>
+   );
+}
+
+export default NavBar;
---
+++ e-commerce-spa/src/components/Sidebar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function SideBar(){
+    return (
+        <>
+         <div className="sidebar-container">
+            <ul>
+            <div className="home">Home</div>
+            <div className="about">About</div>
+            </ul>
+        
+      </div>
+        </>
+ );
+    
+}
+
+export default SideBar;
