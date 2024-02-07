2d725ea91b0cde3f6e5f7d51542f6d09846c1b89
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-07 23:52:00 +0100

    Add new StudentList component

---
+++ e-commerce-spa/src/components/StudentList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import students from "../data/students.json"
+
+function Students(){
+return(
+    <>
+    </>
+)
+
+
+}
+
+export default Students;
