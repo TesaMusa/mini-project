c7b19592fda29063d17b571abbf859a54fb109fd
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-08 00:52:47 +0100

    Display the list of students and render them, work in progress

--- e-commerce-spa/src/components/StudentList.jsx
+++ e-commerce-spa/src/components/StudentList.jsx
@@ -1,14 +1,50 @@
 import React from "react";
 import { useState } from "react";
-import students from "../data/students.json"
+import students from "../data/students.json";
 
-function Students(){
-return(
+function Students() {
+  const [studentsToDisplay, setStudents] = useState(students);
+
+  // Deleting students
+  const deleteStudents = (studentId) => {
+    const newList = studentsToDisplay.filter((studentObj) => {
+      return studentObj.id !== studentId;
+    });
+    setStudents(newList);
+  };
+
+  return (
     <>
+      <section>
+        <h1 className="my-students">Students list</h1>
+       
+        <table>
+          <thead>
+            <tr>
+              <th>Last Name</th>
+              <th>Gender</th>
+              <th>Email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {studentsToDisplay.map((element) => (
+              <tr key={element.id}>
+                <td>{element.last_name}</td>
+                <td>{element.gender}</td>
+                <td>{element.email}</td>
+                <td>
+                  <button onClick={() => deleteStudents(element.id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
     </>
-)
-
-
+  );
 }
 
 export default Students;
