af5982868e3b4455b1cc3c3e73953faa92c4dbd5
Rui Duram <ruiduran.jr@gmail.com>
2024-02-13 18:08:02 +0100

    implement changes to css and merge with changes from fellow developer

--- e-commerce-spa/src/components/Products.jsx
+++ e-commerce-spa/src/components/Products.jsx
@@ -3,9 +3,50 @@ import productList from "../data/ProductList.json";
 import { useNavigate } from "react-router";
 
 function Products() {
+  const navigate = useNavigate();
   const [productsToDisplay, setProducts] = useState(productList);
   const [lightingProducts, setLightingProducts] = useState([]);
-  const navigate = useNavigate();
+  const [productName, setProductName] = useState("");
+  const [productDescription, setProductDescription] = useState("");
+  const [productPrice, setProductPrice] = useState("");
+  const [productRating, setProductRating] = useState("");
+  const [productImage, setProductImage] = useState("");
+
+  /*const checkFormValidity = () => {
+    return (
+      productName !== "" &&
+      productDescription !== "" &&
+      productPrice !== "" &&
+      productRating !== "" &&
+      productImage !== ""
+    );
+  }; */
+
+  // Adding Products
+  //const handleSubmit = (event) => {
+  // if (!checkFormValidity) {
+  //   return;
+  // }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newProduct = {
+      productName: productName,
+      productDescription: productDescription,
+      productPrice: productPrice,
+      productRating: productRating,
+      productImage: productImage,
+    };
+    console.log("Submitted", newProduct);
+    const newProductList = [newProduct, ...productsToDisplay];
+    setProducts(newProductList);
+
+    setProductName("");
+    setProductDescription("");
+    setProductPrice(null);
+    setProductRating(null);
+    setProductImage("");
+  };
+
   // Deleting products
   const deleteProduct = (productId) => {
     const newList = productsToDisplay.filter(
@@ -22,47 +63,115 @@ function Products() {
     setLightingProducts(lighting);
   };
 
+  // the image label is still unfinished. not sure on how to allow passing URLs for the user
   return (
-    <>
-      <section>
-        <h1 className="my-products">Products List</h1>
+    <section>
+      <form onSubmit={handleSubmit}>
+        <span>Add a product</span>
+        <div>
+          <label>
+            Product
+            <input
+              name="productName"
+              type="text"
+              placeholder="Product"
+              value={productName}
+              onChange={(e) => {
+                setProductName(e.target.value);
+              }}
+            />
+          </label>
 
-        <div className="products-container">
-          {productsToDisplay.map((product) => (
-            <div key={product.id} className="product-item">
-              <img
-                className="image-products"
-                src={product.thumbnail}
-                alt={product.title}
-                onClick={() => {
-                  navigate(`/details/${product.id}`);
-                }}
-              />
-              <div className="productsText">
-                <h3>{product.title}</h3>
-                <p>{product.description}</p>
-                <p>Price: ${product.price}</p>
-                <p>Rating: {product.rating}</p>
-                <button onClick={() => deleteProduct(product.id)}>
-                  Delete
-                </button>
-              </div>
-            </div>
-          ))}
+          <label>
+            Description
+            <input
+              name="productDescription"
+              type="text"
+              placeholder="Description"
+              value={productDescription}
+              onChange={(e) => {
+                setProductDescription(e.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Price
+            <input
+              name="productPrice"
+              type="number"
+              placeholder="Price"
+              value={productPrice}
+              onChange={(e) => {
+                setProductPrice(e.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Rating
+            <input
+              name="productRating"
+              type="number"
+              placeholder="Rating"
+              value={productRating}
+              onChange={(e) => {
+                setProductRating(e.target.value);
+              }}
+            />
+          </label>
+
+          <label>
+            Image
+            <input
+              name="productImage"
+              type="text"
+              placeholder="image"
+              value={productImage}
+              onChange={(e) => {
+                setProductImage(e.target.value);
+              }}
+            />
+          </label>
         </div>
-        <button onClick={showLightingProducts}>Show Lighting Products</button>
-        {lightingProducts.length > 0 && (
-          <div>
-            <h2>Lighting Products</h2>
-            <ul>
-              {lightingProducts.map((product) => (
-                <li key={product.id}>{product.title}</li>
-              ))}
-            </ul>
+        <button type="submit">Add Product</button>
+      </form>
+
+      <h1 className="my-products">Products List</h1>
+
+      <div className="products-container">
+        {productsToDisplay.map((product) => (
+          <div key={product.id} className="product-item">
+            <img
+              className="image-products"
+              src={product.thumbnail}
+              alt={product.title}
+              onClick={() => {
+                navigate(`/details/${product.id}`);
+              }}
+            />
+            <div className="productsText">
+              <h3>{product.title}</h3>
+              <p>{product.description}</p>
+              <p>Price: ${product.price}</p>
+              <p>Rating: {product.rating}</p>
+              <button onClick={() => deleteProduct(product.id)}>Delete</button>
+            </div>
           </div>
-        )}
-      </section>
-    </>
+        ))}
+      </div>
+      <button onClick={showLightingProducts}>Show Lighting Products</button>
+      {lightingProducts.length > 0 && (
+        <div>
+          <h2>Lighting Products</h2>
+          <ul>
+            {lightingProducts.map((product) => (
+              <li key={product.id}>{product.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </section>
   );
 }
 
