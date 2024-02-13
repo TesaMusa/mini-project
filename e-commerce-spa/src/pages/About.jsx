9f0341b3895e2a7f9c81bde17d2817d632f22159
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-14 00:28:52 +0100

    Add short description in About page

--- e-commerce-spa/src/pages/About.jsx
+++ e-commerce-spa/src/pages/About.jsx
@@ -6,15 +6,17 @@ function About() {
       <div className="names">
         <div>
           <h2>Rui</h2>
-          <p>talking random crap</p>
+          <p>CSS did not give him enough time to write about himself</p>
         </div>
         <div>
           <h2>Tesa</h2>
-          <p>talking even more random</p>
+          <p>If she were the genius behind a programming language,
+            <br/> she’d probably name it after a feline friend,
+            <br/>  echoing her love for CATS in the world of technology</p>
         </div>
         <div className="ari">
           <h2>Ari</h2>
-          <p>what happens if i write even more retarded stuff here</p>
+          <p>Out in the world  to add one more step to his glorious career path from artist, carpenter, teacher, Jiu-Jitsu Fighter, again artist, carpenter, bookdealer and…….Webdev??!!</p>
         </div>
       </div>
     </div>
