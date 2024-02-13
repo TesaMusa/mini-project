07442bbba5a00bd2548b77bf62a26ce24581c5ef
Rui Duram <ruiduran.jr@gmail.com>
2024-02-14 00:38:45 +0100

    changes to about us page

--- e-commerce-spa/src/pages/About.jsx
+++ e-commerce-spa/src/pages/About.jsx
@@ -3,18 +3,30 @@ function About() {
   return (
     <div>
       <h1 className="about">About Us</h1>
+      <div className="project-info"> Our project:</div>
       <div className="names">
-        <div>
+        <div className="ari">
           <h2>Rui</h2>
-          <p>talking random crap</p>
+          <p>
+            Ex flight attendant that got tired of flying around and decided to
+            ground his feet into the Webdev world link to my github:
+          </p>
         </div>
-        <div>
+        <div className="ari">
           <h2>Tesa</h2>
-          <p>talking even more random</p>
+          <p>
+            If she were the genius behind a programming language, she’d probably
+            name it after a feline friend, echoing her love for cats in the
+            world of technology link to my github: https://github.com/TesaMusa
+          </p>
         </div>
         <div className="ari">
           <h2>Ari</h2>
-          <p>what happens if i write even more retarded stuff here</p>
+          <p>
+            Out in the world to add one more step to his glorious career path
+            from artist, carpenter, teacher, Jiu-Jitsu Fighter, again artist,
+            carpenter, bookdealer and…….Webdev??!! link to my github:
+          </p>
         </div>
       </div>
     </div>
